04ff4732b1d020df4639723e38eb38589e569c10
Kordian Janowski <kordianj1227@gmail.com>
2022-04-20 23:24:05 +0200

    Delete secret.js

--- frontend/secret.js
+++
@@ -1,4 +0,0 @@
-export const CLOUDINARY_API_KEY = '453499119442835'
-export const CLOUDINARY_API_SECRET = 'gos0bzFpuPtkvIIn7W3cpo6rf9c'
-export const CLOUDINARY_CLOUD_NAME = 'dh35iucxu'
-export const CLOUDINARY_UPLOAD_PRESET = 'qpfb0fma'
